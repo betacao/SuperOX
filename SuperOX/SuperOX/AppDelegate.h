e79cae367e0ad97e0e7500ef4d16330c4d8d8063
曹长喜 <906689960@qq.com>
2016-05-17 15:23:22 +0800

    上传代码

---
+++ SuperOX/SuperOX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperOX
+//
+//  Created by changxicao on 16/5/17.
+//  Copyright © 2016年 changxicao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperOX/SuperOX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperOX
+//
+//  Created by changxicao on 16/5/17.
+//  Copyright © 2016年 changxicao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
